b5fa79d3b4612945436bf44c4b5c026278867716
Bradley Latreille <bradltr95@gmail.com>
2025-06-18 00:12:37 -0400

    refac: enhance page object element checking
    refactor: simplify element existence checks in CartPage and InventoryPage by using checkElement.

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -3,19 +3,9 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-/*
-Cart Page Purpose:
-- Display items in the cart.
-- Remove items from the cart.
-- Proceeding to checkout.
- */
-// Navigate to cart from the inventory page.
-// Checkout from cart.
-// Continue shopping (This is a button that takes you back to the inventory page).
-// THESE ELEMENTS ARE PART OF THE ITEM IN THE CART
-// - Quantity, Remove Button, Price, Description, Page Header ("Your Cart")
-// Select cart items (This is a link in the cart item header that takes you to the item description
+// The Cart Page Object Model
 public class CartPage extends BasePage {
+    // Element identifiers
     private final By inventoryItemName = By.className("inventory_item_name");
     private final By inventoryItemDescription = By.className("inventory_item_description");
     private final By inventoryItemPrice = By.className("inventory_item_price");
@@ -25,12 +15,10 @@ public class CartPage extends BasePage {
         super(driver, cartPath);
     }
 
-    public boolean isItemNameDisplayed() {
-        return checkElement.elementExists(inventoryItemName);
-    }
-    public boolean isItemDescriptionDisplayed() { return  driver.findElement(inventoryItemDescription).isDisplayed(); }
-    public boolean isItemPriceDisplayed() { return  driver.findElement(inventoryItemPrice).isDisplayed(); }
-    public boolean isRemoveBackpackDisplayed() { return  driver.findElement(removeSauceLabsBackpackButton).isDisplayed(); }
+    public boolean isItemNameDisplayed() { return checkElement.elementExists(inventoryItemName); }
+    public boolean isItemDescriptionDisplayed() { return checkElement.elementExists(inventoryItemDescription); }
+    public boolean isItemPriceDisplayed() { return checkElement.elementExists(inventoryItemPrice); }
+    public boolean isRemoveBackpackDisplayed() { return checkElement.elementExists(removeSauceLabsBackpackButton); }
 
     public void removeCartItem(String item) {
         // We can use a unique identifier here such as the header of the item
