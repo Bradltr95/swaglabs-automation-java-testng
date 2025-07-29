613c2071665315b6943546c03f79d81ffd2f1628
Bradley Latreille <bradltr95@gmail.com>
2025-07-28 23:24:24 -0400

    refactor: remove implicit waits and unused imports
    - Remove the implicit waits no longer necessary as we make use of fluent waits in the WaitUtil.java class
    - Remove unused imports

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,12 +3,9 @@ package tests;
 import driver.DriverType;
 import driver.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Parameters;
-
-import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -17,7 +14,6 @@ public class BaseTest {
     @BeforeMethod
     public void setup(String baseUrl) {
         driver = WebDriverFactory.createDriver(DriverType.CHROME);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get(baseUrl);
     }
